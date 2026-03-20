b4b0effc74c107aba3c42cbd328f4312c177817c
Charlyken <keunitchokonthe@gmail.com>
2026-03-20 10:45:51 +0100

    feat(invoice): Amélioration de l'accessibilité
     - Ajout du eslint plugin pour implementer les meilleures pratiques d'accessibilité
    - Implementation de la validation coté server pour ameliorer la performance de l'app et l'integrité des données: Celle-ci s'est faite via le hook useActionState pour le creation du formulaire, mais pas pour l'edit (une erreur fut rencontrée - à corriger à la prochaine session de codage)

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -10,23 +10,50 @@ const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 // Define a Zod schema for the form data
 const FormSchema = z.object({
     id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
+    customerId: z.string({
+        invalid_type_error: 'Please select a customer.',
+    }),
+    amount: z.coerce
+        .number()
+        .gt(0, { message: 'Please enter an amount greater than $0.' }),
+    status: z.enum(['pending', 'paid'], {
+        invalid_type_error: 'Please select an invoice status.',
+    }),
     date: z.string(),
 });
 
 // Create a new type that omits the 'id' and 'date' fields for the create action
 const CreateInvoice = FormSchema.omit({id: true, date: true});
 
+export type State = {
+    errors?: {
+        customerId?: string[];
+        amount?: string[];
+        status?: string[];
+
+    };
+    message?: string | null;
+
+};
+
 // Server action to create a new invoice
-export async function createInvoice (formData: FormData){
+export async function createInvoice (prevState: State, formData: FormData){
     // Validate and parse the form data using Zod
-    const { customerId, amount, status } = CreateInvoice.parse({
+    const validatedFields = CreateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
+
+    // If form validation fails, return errors early. Otherwise, continue.
+    if(!validatedFields.success){
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Failed to Create Invoice.',
+        };
+    }
+
+    const {customerId, amount, status} = validatedFields.data;
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
